5838bcb2c7fac79db715c5ad55c13d4b7a81cc58
TuHue <94226385+TuHue@users.noreply.github.com>
2021-12-10 11:52:23 +0700

    Create Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from "react";
 
+import { Article, Brand, CTA, Feature, Navbar } from "./components";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+import './App.css';
 function App() {
-
-    return(
-        <h1>Hello</h1>
-    )
+  return (
+    <div className="App">
+      <div className="gradient_bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/article/Article.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './article.css';
+
+function Article(){
+
+}
+
+export default Article;
